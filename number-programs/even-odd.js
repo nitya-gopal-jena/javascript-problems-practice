4f20f57cc7a306a8cc0be0329fee0840960d12fe
Nityagopaljena98 <nityagjena@hotmail.com>
2026-05-27 19:09:10 +0530

    Added even/odd number program

---
+++ number-programs/even-odd.js
@@ -0,0 +1,13 @@
+
+// Check the given number is even or odd ?
+
+function evenOdd() {
+    let num = 153;
+    if (num % 2 == 0) {
+        console.log(`${num} is even`);
+    } else {
+        console.log(`${num} is odd`);
+    }
+}
+
+evenOdd();
